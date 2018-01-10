083076315388b210c2a820b2c46028e7b1970cdb
behnoush norouzali <behnoush.norouzi@gmail.com>
2018-01-10 17:17:56 +0100

    add a new evenType

--- src/event-lib/EventType.php
+++ src/event-lib/EventType.php
@@ -9,4 +9,5 @@ final class EventType extends AbstractEnum
     const CREATED = 'created';
     const UPDATED = 'updated';
     const DELETED = 'deleted';
+    const ASK_CONFIRMATION_CODE = 'ask_confirmation_code';
 }
